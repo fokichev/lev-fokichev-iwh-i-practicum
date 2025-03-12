ad1f794bc6e09dd61c0c5f52ec96572a814a83c6
Polina <fokichevap@gmail.com>
2025-03-12 13:40:38 +0000

    Fetch list of custom object (empty array at this stage)

--- index.js
+++ index.js
@@ -10,10 +10,27 @@ app.use(express.json());
 
 // * Please DO NOT INCLUDE the private app access token in your repo. Don't do this practicum in your normal account.
 const PRIVATE_APP_ACCESS = process.env.PRIVATE_APP_KEY;
+const config = {
+    objectType: 'p_ghibli_movies',
+    headers: {
+        Authorization: `Bearer ${PRIVATE_APP_ACCESS}`,
+        'Content-Type': 'application/json'
+    }
+}
 
 // TODO: ROUTE 1 - Create a new app.get route for the homepage to call your custom object data. Pass this data along to the front-end and create a new pug template in the views folder.
 
 // * Code for Route 1 goes here
+app.get('/', async (req, res) => {
+    const route = `https://api.hubspot.com/crm/v3/objects/${config.objectType}`;
+    try {
+        const response = await axios.get(route, { headers: config.headers });
+
+        res.json(response.data.results);
+    } catch (error) {
+        console.error(error);
+    }
+});
 
 // TODO: ROUTE 2 - Create a new app.get route for the form to create or update new custom object data. Send this data along in the next route.
 
